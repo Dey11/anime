a6a3b5e2591dda53a9b698a830278e3187ccedab
Dey11 <shreyandey11@gmail.com>
2024-05-12 07:25:42 +0530

    add explore page

--- src/app/anime/page.tsx
+++ src/app/anime/page.tsx
@@ -1,8 +1,33 @@
-import Image from "next/image";
-import React from "react";
+import CarouselComponent from "@/components/explore/carousel";
+import RecentEps from "@/components/explore/recenteps";
+import TopAiring from "@/components/explore/topairing";
+import { Metadata } from "next";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Explore Anidey",
+  description:
+    "Browse through a carefully curated collection of featured, top airing and recent anime episodes",
+};
 
 const page = () => {
-  return <div>hello world</div>;
+  return (
+    <div className="px-2">
+      <div className="">
+        <CarouselComponent />
+      </div>
+      <div>
+        <Suspense fallback={"Loading"}>
+          <TopAiring />
+        </Suspense>
+      </div>
+      <div>
+        <Suspense fallback={"Loading"}>
+          <RecentEps />
+        </Suspense>
+      </div>
+    </div>
+  );
 };
 
 export default page;
