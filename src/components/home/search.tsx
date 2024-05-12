ecacfb846f70d2401804d3e09badb863b4d0e44a
Dey11 <shreyandey11@gmail.com>
2024-05-13 00:28:11 +0530

    fix a non responsive search bar

--- src/components/home/search.tsx
+++ src/components/home/search.tsx
@@ -15,17 +15,17 @@ const SearchSection = () => {
         </p>
       </div>
       <form
-        className="col-span-3 grid grid-cols-3"
+        className="col-span-3 grid grid-cols-2 justify-between"
         action={(formData: FormData) => {
           const query = formData.get("query") as string;
           if (!query) return;
           router.push(`/anime/search?query=${query}`);
         }}
       >
-        <div className="col-span-2">
+        <div className="col-span-1">
           <Input placeholder="Jujutsu Kaisen" name="query" />
         </div>
-        <div className="mt-5 md:mt-0 text-center">
+        <div className="md:mt-0 text-center">
           <Button variant={"outline"} className="px-10 md:px-5">
             Search
           </Button>
