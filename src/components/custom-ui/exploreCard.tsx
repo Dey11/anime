a36e649c3762ecdd8abde3582898cd82175d6548
Dey11 <shreyandey11@gmail.com>
2024-05-12 07:26:55 +0530

    card component used in explore page

---
+++ src/components/custom-ui/exploreCard.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export type ExplorePageCardProps = {
+  id: string;
+  title: string;
+  image: string;
+  episodeId: string;
+  episodeNumber: number;
+};
+
+const ExplorePageCard = ({
+  id,
+  title,
+  image,
+  episodeId,
+  episodeNumber,
+}: ExplorePageCardProps) => {
+  return (
+    <Link href={`/anime/stream/${id}/${episodeId}`}>
+      <div
+        className="md:w-52 md:h-72 w-32 h-52 relative rounded-md border border-gray-300
+         overflow-hidden transition-transform duration-50 transform-gpu hover:scale-105"
+      >
+        <Image
+          className="object-cover"
+          src={image}
+          fill
+          alt={title}
+          sizes="fill"
+          priority={true}
+        />
+        <div
+          className="absolute bottom-0 left-0 right-0 text-center p-2 
+        bg-black bg-opacity-65
+        text-white"
+        >
+          <div className="text-sm md:text-lg pb-2">{title}</div>
+          <div className="text-sm">Episode:{` ${episodeNumber}`}</div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ExplorePageCard;
