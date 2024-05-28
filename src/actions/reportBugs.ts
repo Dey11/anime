33ff4954fa70d77698e7822cb735f5ad1214bc21
Dey11 <shreyandey11@gmail.com>
2024-05-28 23:25:28 +0530

    edit report bugs server action

--- src/actions/reportBugs.ts
+++ src/actions/reportBugs.ts
@@ -9,10 +9,17 @@ type reportBugProps = {
 
 async function reportBug({ username, description, url }: reportBugProps) {
   try {
-    // const newBug = await
+    const newBug = await prisma.errors.create({
+      data: {
+        username,
+        description,
+        url,
+      },
+    });
+
+    console.log(newBug);
   } catch (err) {
     console.log(err);
-
     return -1;
   }
 }
