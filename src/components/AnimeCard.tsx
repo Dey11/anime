8ea078ea966f7be190552f4d462809d8134ae589
Dey11 <shreyandey11@gmail.com>
2024-06-01 08:07:35 +0530

    prettify search page

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -35,8 +35,8 @@ const AnimeCard = ({
   return (
     <Link href={`/anime/info/${id}`}>
       <div
-        className="duration-50 relative h-52 w-32 transform-gpu overflow-hidden rounded-md border
-        border-gray-300 transition-transform hover:scale-105 md:h-72 md:w-52"
+        className="duration-50 relative h-48 w-28 transform-gpu overflow-hidden rounded-md
+        border border-gray-300 transition-transform hover:scale-105 md:m-2 md:h-72 md:w-52"
       >
         <Image
           className="object-cover"
@@ -46,13 +46,20 @@ const AnimeCard = ({
           sizes="fill"
           priority={true}
         />
+
         <div
-          className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-65 
-        p-2 text-center
-        text-white"
+          className="hover:shadow-3xl absolute left-0 top-0 z-10 h-48 
+        w-28 items-center justify-center bg-transparent hover:bg-black
+        hover:bg-opacity-30 md:h-72 md:w-52"
         >
-          <div className="font-semibold">{title}</div>
-          <div className="  text-xs">{releaseDate}</div>
+          <div
+            className="absolute bottom-0 left-0 right-0
+        bg-black bg-opacity-65 p-2
+        text-center text-white"
+          >
+            <div className="pb-2 text-sm md:text-lg">{title}</div>
+            <div className="text-sm">{releaseDate}</div>
+          </div>
         </div>
       </div>
     </Link>
